c0136992caff4f47c91b4aba0c678e0ccea4e154
saifudnm <rullyudin234@gmail.com>
2023-10-21 16:25:13 +0700

    anything

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -3,4 +3,4 @@ import matplotlib.pyplot as plt
 
 df = pd.DataFrame({"A":[1,2,3], "B":[1,2,3]})
 
-df2 = pd.DataFrame({"A":[1,2,3,4], "B":[1,2,3,4]})
+df2 = pd.DataFrame({"A":[1,2,3,4,5], "B":[1,2,3,4,5]})
